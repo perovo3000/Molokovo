b3d8361b5073e830ced4498d2f90731f6a70b142
RB <funk@bk.ru>
2022-04-28 17:10:20 +0300

    Calk3

--- calculator.py
+++ calculator.py
@@ -1,16 +1,18 @@
 import tkinter as tk
 
+
 def get_data():
-    s=[]
+    s = []
     s1 = int(ent1.get())
     s.append(s1)
     s2 = int(ent2.get())
     s.append(s2)
     return s
 
+
 def sum1():
-    k=get_data()
-    rezultat=k[0]+k[1]
+    k = get_data()
+    rezultat = k[0] + k[1]
     lab1['text'] = rezultat
 
 
@@ -22,7 +24,7 @@ def umn():
 
 def del1():
     k = get_data()
-    rezultat = k[0] /k[1]
+    rezultat = k[0] / k[1]
     lab1['text'] = rezultat
 
 
@@ -31,15 +33,17 @@ def minus():
     rezultat = k[0] / k[1]
     lab1['text'] = rezultat
 
+
 root = tk.Tk()
-lab = tk.Label(text='Caculator')
-lab1=tk.Label(width=20)
-ent1 = tk.Entry(width=20)
+# root.geometry()
+lab = tk.Label(text="Calculator")
+lab1 = tk.Label(width=6, height=4)
+ent1 = tk.Entry(width=20, font='arial 10')
 ent2 = tk.Entry(width=20)
-b1 = tk.Button(text="+")
-b2 = tk.Button(text="-")
-b3 = tk.Button(text='/')
-b4 = tk.Button(text='*')
+b1 = tk.Button(text="+", font=16, width=4)
+b2 = tk.Button(text="-", font=16, width=4)
+b3 = tk.Button(text='/', font=16, width=4)
+b4 = tk.Button(text='*', font=16, width=4)
 b1['command'] = sum1
 b2['command'] = minus
 b3['command'] = del1
