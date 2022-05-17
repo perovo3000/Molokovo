15f74ea2ed0957e82d2d3a84d478b01bd0533a12
RB <funk@bk.ru>
2022-05-17 18:40:14 +0300

    Calk3

--- test.py
+++ test.py
@@ -1,14 +1,39 @@
 import tkinter as tk
+from random import randint
+
+WIDTH = 600
+HEIGHT = 400
 
 
 def tick():
-    canvas.move(krug, +10, +10)
-    root.after(50, tick)
+    global ball, canvas, root, x, y, R, dx, dy
+    x += dx
+    y += dy
+    print('x= ', x, "   y= ", y)
+    if x - R <= 0 or x + R > WIDTH:
+        dx = -dx
+    if y - R <= 0 or y + R > HEIGHT:
+        dy = -dy
+    canvas.move(ball, dx, dy)
+    root.after(20, tick)
 
 
-root = tk.Tk()
-root.geometry('600x400')
-canvas = tk.Canvas(root)
-canvas.pack(anchor='nw')
-krug = canvas.create_oval(100, 100, 200, 200, fill='green')
-root.mainloop()
+def main():
+    global ball, canvas, root, x, y, R, dx, dy
+    dx, dy = [1, 1]
+    root = tk.Tk()
+    root.title('Boll')
+    root.geometry(str(WIDTH) + "x" + str(HEIGHT))
+    canvas = tk.Canvas(root)
+    canvas.pack(anchor='center')
+    R = randint(30, 40)
+    x = randint(R, WIDTH - R)
+    y = randint(R, HEIGHT - R)
+    print('R=', R, "  x=", x, "  y=", y)
+    ball = canvas.create_oval(x - R, y - R, x + R, y + R, fill='green')
+    tick()
+    root.mainloop()
+
+
+if __name__ == "__main__":
+    main()
