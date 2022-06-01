36ebdbec3c15e215cfd8867b4d261e9f083301f6
RB <funk@bk.ru>
2022-06-01 13:03:36 +0300

    Class from work

--- 2.py
+++ 2.py
@@ -1,7 +1,12 @@
+def import_list(name):
+    l = name
+    return l
+
+
 a = [1, 2, 3, 4, 5, 6, 7, 8, 8]
-b=[]
+b = []
 for i in a:
-    b.append(i**2)
-print(b)
-
-
+    b.append(i ** 2)
+c = import_list(b)
+print(c)
+# возврат функцией списка и его печать
