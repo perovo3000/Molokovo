74e1f0fc7701bdf76cd9d060095dda7b262c7cef
RB <funk@bk.ru>
2023-01-11 16:32:08 +0300

    Рабочая версия

--- print_skrin.py
+++ print_skrin.py
@@ -6,13 +6,13 @@ import time
 namber_screen = int(input("Вести число страниц   "))
 for i in range(1, namber_screen + 1):
     screen = 'scr_' + str(i) + '.png'
-# вводим адрес страницы для принтскрина i - меняющийся номер HTML страницы
-    http = 'https://sdo.itsecurity.ru/UCV3/BT01_v7Thj/'+str(i+1)+'/index.html'
-# открываем необходимую страницу в браузере по умолчанию
+    # вводим адрес страницы для принтскрина i - меняющийся номер HTML страницы
+    http = 'https://sdo.itsecurity.ru/UCV3/KP06_v7_h5LLwee/' + str(i + 1) + '/index.html#zoom=z'
+    # открываем необходимую страницу в браузере по умолчанию
     webbrowser.open(http, new=0)
-# необходимо время задержки, для отурытия страницы в браузере
-    time.sleep(5)
-# делаем непосредственно printscreen
-    pyautogui.screenshot(screen, region=(100, 100, 1800, 1000))
+    # необходимо время задержки, для отурытия страницы в браузере
+    time.sleep(3)
+    # делаем непосредственно printscreen
+    pyautogui.screenshot(screen)
 
-#!!!! ALT - Tab для переключение между браузером и запуска Scrept
+# !!!! ALT - Tab для переключение между браузером и запуска Scrept
